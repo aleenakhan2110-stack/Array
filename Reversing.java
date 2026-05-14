431a9dbdefee71ad0a8f46d32f75348be5ae5161
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-05-14 19:59:39 +0530

    Add files via upload

--- Reversing.java
+++ Reversing.java
@@ -4,14 +4,14 @@ public class Reversing {
          Scanner sc=new Scanner(System.in);
          int N=sc.nextInt();
          long [] arr=new long [N];
-         for(int i=0;i<=N;i++){
+         for(int i=0;i<N;i++){
              arr[i]=sc.nextInt();
 
          }
-         for(int i=N-1;i<=0;i++){
-             System.out.println(arr[i]);
+         for(int i=N-1;i>=0;i--){
+             System.out.print(arr[i]);
              if(i>0){
-                 System.out.println(" ");
+                 System.out.print(" ");
              }
          }
     }
